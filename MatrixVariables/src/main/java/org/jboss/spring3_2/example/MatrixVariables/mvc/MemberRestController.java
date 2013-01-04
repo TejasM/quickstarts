c119194ed1d8a7bf20ce5401942780675bcc5629
Tejas <tmehta@redhat.com>
2013-01-04 16:32:44 -0500

    Matrix Variables example added but not fully functional

---
+++ MatrixVariables/src/main/java/org/jboss/spring3_2/example/MatrixVariables/mvc/MemberRestController.java
@@ -0,0 +1,33 @@
+package org.jboss.spring3_2.example.MatrixVariables.mvc;
+
+import java.util.List;
+
+import org.jboss.spring3_2.example.MatrixVariables.domain.Member;
+import org.jboss.spring3_2.example.MatrixVariables.repo.MemberDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.MatrixVariable;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/rest/members")
+public class MemberRestController
+{
+    @Autowired
+    private MemberDao memberDao;
+
+    @RequestMapping(method=RequestMethod.GET, produces="application/json")
+    public @ResponseBody List<Member> listAllMembers()
+    {
+        return memberDao.findAllOrderedByName();
+    }
+
+    @RequestMapping(value="/{id}", method=RequestMethod.GET, produces="application/json")
+    public @ResponseBody Member lookupMemberById(@PathVariable("id") Long id)
+    {
+        return memberDao.findById(id);
+    }
+}
